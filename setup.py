5e279f0f48be510592000327c188aac31c54dc67
Frank Martin <frank@grafikus.nl>
2023-03-10 10:55:46 +0100

    Python version update

--- setup.py
+++ setup.py
@@ -6,14 +6,14 @@ setup(
     description='Python wrapper for Circe',
     long_description='Python wrapper for Circe',
     long_description_content_type='text/markdown',
-    version='0.0.3',
+    version='0.0.4',
     packages=find_namespace_packages(),
     include_package_data=True,
     package_data={
         'circe.java': ['*.jar'],
         'circe.data': ['*.json']
     },
-    python_requires='>=3.9',
+    python_requires='>=3.10',
     install_requires=[
         'jpype1==1.4.1',
     ]
